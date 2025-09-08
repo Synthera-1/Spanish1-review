62d8db061d26f36a582adeff084a69c3b96eab0e
MeCode <164209877+Synthera-1@users.noreply.github.com>
2025-09-08 10:27:37 -0500

    Update alphabet.js

--- alphabet.js
+++ alphabet.js
@@ -1,3 +1,33 @@
 // alphabet.js
-::contentReference[oaicite:0]{index=0}
- 
+
+const alphabet = [
+  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+];
+
+const greetings = [
+  "Hola", "Adiós", "Buenos días", "Buenas tardes", "Buenas noches", "¿Cómo estás?", "¿Qué tal?", "¿Cómo te va?"
+];
+
+function startAlphabetGame() {
+  const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
+  const greetingLetters = randomGreeting.split("");
+  let currentLetterIndex = 0;
+
+  function displayLetter() {
+    if (currentLetterIndex < greetingLetters.length) {
+      const letter = greetingLetters[currentLetterIndex];
+      const letterIndex = alphabet.indexOf(letter.toUpperCase());
+      if (letterIndex !== -1) {
+        console.log(`Letra: ${letter} - Índice: ${letterIndex}`);
+      }
+      currentLetterIndex++;
+      setTimeout(displayLetter, 1000);
+    } else {
+      console.log("¡Juego terminado!");
+    }
+  }
+
+  displayLetter();
+}
+
+startAlphabetGame();
